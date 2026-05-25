c78869ed3fb75e98760f1f3c75aa5528866f467c
d3fpy <136571437+d3fpy@users.noreply.github.com>
2026-05-25 23:49:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template("index.html")
+
+@app.route('/double')
+def double():
+    return render_template("double.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
